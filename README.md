dc181796bb52c86f136c8440d2971cb3abab5071
Mohebullah Mir <mohebullahmir@gmail.com>
2022-09-02 11:21:03 -0400

    Finished 2 different methods

---
+++ README.md
@@ -0,0 +1,12 @@
+# Rotating Linked List
+## Given the head of a linked list, rotate the list to the right by k places.
+
+### Example
+```
+Input: head = [1, 2, 3, 4, 5], k=2
+Output: [4, 5, 1, 2, 3]
+
+[1, 2, 3, 4, 5]
+rotate 1 -> [5, 1, 2, 3, 4]
+rotate 2 -> [4, 5, 1, 2, 3]
+```
---
+++ rotate.js
@@ -0,0 +1,69 @@
+//Superior version
+var rotateRight = function (head, k) {
+  //Calculate length of List
+  let length = function (h) {
+    let l = 0;
+    while (h !== null) {
+      h = h.next;
+      l++;
+    }
+    return l;
+  };
+  let l = length(head);
+  //Calculate the distance to the new head of list, subtract the length of list by amount of rotations. (k%l) denotes how many rotations we are performing. Because any amount of rotations larger than list size will be denoted by k%length of list.
+  let distance = l - (k % l);
+  //Rotation function, if distance = length of list or head is null, don't do anything.
+  let rotate = function (h) {
+    if (h === null || distance === l) return h;
+    let fast = h;
+    let prev = h;
+    let newhead = h;
+    //Keep traversing list until distance==0 aka we reached new head.
+    while (distance !== 0) {
+      if (distance === 1) prev = fast; //previous node that was pointing to new head
+      fast = fast.next;
+      distance--;
+    }
+    newhead = fast;
+    //Find the tail of old list to point to old head
+    while (fast.next !== null) {
+      fast = fast.next;
+    }
+    fast.next = h;
+    //set prev node as new tail
+    prev.next = null;
+    return newhead;
+  };
+  return rotate(head);
+};
+
+//Slower version, rotating, one at a time , so calling rotate k % length times.
+// var rotateRight = function(head, k) {
+//     let length = function(h){
+//         let l=0
+//         while(h!==null){
+//             h=h.next
+//             l++
+//         }
+//         return l
+//     }
+//     let rotate = function(h){
+//         if(h===null) return h;
+//         let fast = h
+//         let prev=h;
+//         while(fast.next!==null){
+//             if(fast.next.next===null) prev = fast;
+//             fast = fast.next;
+//         }
+//         fast.next = h
+//         prev.next=null
+//         return fast
+//     }
+//     let result = head
+//     k= k%length(head)
+//     while(k>0){
+//         result = rotate(result)
+//         k--
+//     }
+//     return result
+// };
